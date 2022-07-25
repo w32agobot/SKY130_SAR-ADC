09b2d7021c751b4655d12bafd328e32d29a08ecb
Manuel Moser <moser.manuel@mailbox.org>
2022-07-25 10:37:00 +0000

    lvs script updated to run in batch mode and grab top subcircuits correctly

--- lvs/adc_array_cap_16_lvs.sh
+++ lvs/adc_array_cap_16_lvs.sh
@@ -1,1 +1,1 @@
-netgen -noconsole lvs ../xschem/adc_array_cap/adc_array_cap_16.spice "../mag/adc_array_cap/adc_array_cap_16.spice adc_array_cap_16"
+netgen -batch lvs "../mag/adc_array_cap/adc_array_cap_16.spice adc_array_cap_16" "../xschem/adc_array_cap/adc_array_cap_16.spice adc_array_cap_16" /foss/pdk/sky130A/libs.tech/netgen/sky130A_setup.tcl adc_array_cap_16_lvs.out
