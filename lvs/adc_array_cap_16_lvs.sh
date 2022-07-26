90683612ffd366dd10a8c8078830df4887f34357
Manuel Moser <moser.manuel@mailbox.org>
2022-07-26 07:05:33 +0000

    folder structure for novel 150nm structures

--- lvs/adc_array_cap_16_lvs.sh
+++
@@ -1,1 +0,0 @@
-netgen -batch lvs "../mag/adc_array_cap/adc_array_cap_16.spice adc_array_cap_16" "../xschem/adc_array_cap/adc_array_cap_16.spice adc_array_cap_16" /foss/pdk/sky130A/libs.tech/netgen/sky130A_setup.tcl adc_array_cap_16_lvs.out
