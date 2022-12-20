c36b59b9c8bcc10f2a3aca4e6a2a7d8644a79036
w32agobot <userundname@gmail.com>
2022-12-20 17:50:36 +0100

    generate new adc_top with redesigned matrix

--- gds/ext/make_C.tcl
+++ gds/ext/make_C.tcl
@@ -5,40 +5,42 @@ flatten adc_top_flat
 load adc_top_flat
 cellname delete adc_top
 cellname rename adc_top_flat adc_top
-box 958 1966 14090 33170
+box 958 1966 15850 33167
 erase label
-box 928 31990 10252 47956
+box 928 31990 8584 47912
 erase label
-box 928 45718 13852 78604
+box 928 45718 15642 78506
 erase label
-box 12768 46352 46448 53060
+box 12782 45680 48715 53494
 erase label
-box 12816 26726 46448 33088
+box 12813 26867 48755 32866
 erase label
-box 11469 33947 43705 45125
+box 9895 33914 41892 45238
 erase label
-box 48760 53350 48760 53350
+box 43547 26990 48857 53742
+erase label
+box 50560 54060 50560 54060
 label ctopp
 port make
-box 48760 26210 48760 26210
+box 50560 26168 50560 26168
 label ctopn
 port make
-box 51442 73901 51442 73901
+box 53192 57690 53192 57690
 label vcm
 port make
-box 10294 35398 10294 35398
+box 9800 35496 9800 35496
 label clk_dig
 port make
-box 48286 39406 48286 39406
+box 43459 36818 43459 36818
 label clk_comp
 port make
-box 10489 34642 10489 34642
+box 9800 34680 9800 34680
 label clk_ena
 port make
-box 44000 42568 44000 42568
+box 42872 42568 42872 42568
 label ndecision_finish
 port make
-box 55214 40204 55214 40204
+box 57331 40204 57331 40204
 label comp_latch
 port make
 select top cell
@@ -48,7 +50,7 @@ extract warn no fets
 extract all
 ext2spice short resistor
 ext2spice lvs 
-ext2spice cthresh 0.1
+ext2spice cthresh 0.3
 ext2spice merge conservative
 ext2spice
 
