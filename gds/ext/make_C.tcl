e0865dd421f76c7637bf886c0adcbca2f147e082
w32agobot <userundname@gmail.com>
2023-01-17 23:20:43 +0100

    change position of ports

--- gds/ext/make_C.tcl
+++ gds/ext/make_C.tcl
@@ -5,46 +5,48 @@ flatten adc_top_flat
 load adc_top_flat
 cellname delete adc_top
 cellname rename adc_top_flat adc_top
-box 958 1966 15850 33167
+box 1029 2089 15616 78409
 erase label
-box 928 31990 8584 47912
+box 14475 46531 48720 53603
 erase label
-box 928 45718 15642 78506
+box 14475 26866 48720 33550
 erase label
-box 12782 45680 48715 53494
+box 82057 1955 85460 78624
 erase label
-box 12813 26867 48755 32866
+box 17833 54897 52150 73083
 erase label
-box 9895 33914 41892 45238
+box 17833 7143 52150 25329
 erase label
-box 43547 26990 48857 53742
+box 16364 34418 48650 45661
 erase label
 box 50560 54060 50560 54060
 label ctopp
 port make
-box 50560 26168 50560 26168
+box 50500 25799 50500 25799
 label ctopn
 port make
-box 53192 57690 53192 57690
+box 53242 26168 53242 26168
 label vcm
 port make
-box 9800 35496 9800 35496
+box 16300 35996 16300 35996
 label clk_dig
 port make
-box 50807 39378 50807 39378
+box 48900 37084 48900 37084
 label clk_comp
 port make
-box 9800 34680 9800 34680
+box 16300 35180 16300 35180
 label clk_ena
 port make
-box 42872 42568 42872 42568
+box 49700 43068 49700 43068
 label ndecision_finish
 port make
-box 57331 40204 57331 40204
+box 56901 40125 56901 40125
 label comp_latch
 port make
 select top cell
 extract do local 
+extract do aliases 
+extract do labelcheck 
 extract unique
 extract warn no fets
 extract all
