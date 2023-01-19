4d666938468754c4bd6e874b0259642c71e30806
w32agobot <userundname@gmail.com>
2023-01-19 13:09:19 +0100

    update digital xspice and simulation results

--- openlane/adc_core_digital/config.tcl
+++ openlane/adc_core_digital/config.tcl
@@ -2,11 +2,8 @@
 
 set ::env(DESIGN_NAME) adc_core_digital
 
-# technically unused because combinatoric. Values are workarounds.
-# otherwise the flow will fail with STA errors, or because the
-# STA report will not be found although STA is skipped intentionally
- set ::env(CLOCK_PORT) "clk_dig"
- set ::env(CLOCK_NET) "clk_dig"
+ set ::env(CLOCK_PORT) "clk_dig_in"
+ set ::env(CLOCK_NET) "clk_dig_in"
  set ::env(CLOCK_TREE_SYNTH) 1
  set ::env(CLOCK_PERIOD) 20
 
@@ -35,10 +32,6 @@ set ::env(DESIGN_NAME) adc_core_digital
 # Placement
  set ::env(PL_BASIC_PLACEMENT) 0
  set ::env(PL_TARGET_DENSITY) {0.70}
-# set ::env(PL_RESIZER_TIMING_OPTIMIZATIONS) {0}
-# set ::env(PL_RESIZER_DESIGN_OPTIMIZATIONS) {0}
-# set ::env(PL_ROUTABILITY_DRIVEN) {1}
-# set ::env(PL_TIME_DRIVEN) {1}
 
 set ::env(DPL_CELL_PADDING) {2}
 # Router
@@ -52,15 +45,5 @@ if { [file exists $filename] == 1} {
 	source $filename
 }
 
-
-
-# OpenROAD reports unconnected nodes as a warning.
-# OpenLane typically treats unconnected node warnings 
-# as a critical issue, and simply quits.
-#
-# We'll be leaving it up to the designer's discretion to
-# enable/disable this: if LVS passes you're probably fine
-# with this option being turned off.
-# set ::env(FP_PDN_CHECK_NODES) 0
 
 
